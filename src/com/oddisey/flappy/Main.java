ae6dcd8eaf5e35a9a1c0068a80dafbba04d0629d
Vitalii Pertsovych <oddisey000@gmail.com>
2022-05-27 19:47:38 +0300

    Implemented simple OpenGL Rendering

--- src/com/oddisey/flappy/Main.java
+++ src/com/oddisey/flappy/Main.java
@@ -1,6 +1,8 @@
 package com.oddisey.flappy;
 
+import com.oddisey.flappy.input.Input;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -9,28 +11,45 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class Main implements Runnable {
 
     private boolean isRunning = false;
+    private long window;
 
     public void start() {
-        isRunning = false;
+        isRunning = true;
         Thread thread = new Thread(this, "Game");
         thread.start();
     }
 
     private void init() {
         if (!glfwInit()) {
-            // TODO: handle it
             System.out.println("Cannot initialize window!");
+            return;
         }
         glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
         // Application's window size and title
-        int width = 1280;
-        int height = 720;
+        int width = 800;
+        int height = 600;
         String title = "Flappy Bird";
-        long window = glfwCreateWindow(width, height, title, NULL, NULL);
+        window = glfwCreateWindow(width, height, title, NULL, NULL);
+
+        // Checks if window could be build correctly
+        if (window == NULL) {
+            System.out.println("Something bad happened to window");
+            return;
+        }
 
         GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         assert videoMode != null;
-        glfwSetWindowPos(window, videoMode.width(), videoMode.height());
+
+        glfwSetWindowPos(window, (videoMode.width() - width) / 2, (videoMode.height() - height) / 2);
+        glfwSetKeyCallback(window, new Input());
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+
+        GL.createCapabilities();
+
+        glClearColor(1.0f, .0f, 1.0f, 1.0f);
+        glEnable(GL_DEPTH_TEST);
+        System.out.println("OpenGL " + glGetString(GL_VERSION));
     }
 
     public void run() {
@@ -38,11 +57,22 @@ public class Main implements Runnable {
         while (isRunning) {
             update();
             render();
+            if (glfwWindowShouldClose(window)) {
+                isRunning = false;
+            }
         }
     }
 
-    private void update() {}
-    private void render() {}
+    private void update() {
+        glfwPollEvents();
+        if (Input.keys[GLFW_KEY_SPACE]) {
+            System.out.println("Flying!");
+        }
+    }
+    private void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
 
     public static void main(String[] args) {
         new Main().start();
